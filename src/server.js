29924e4cb7c9828f1f05183d2bbe38d3bb5a5603
younbihn <younbihn@naver.com>
2025-01-28 01:11:27 +0900

    오탈자 수정

--- src/server.js
+++ src/server.js
@@ -8,4 +8,4 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res. render ("home"));
 
 const handleListen = () => console.log('Listening on http://localhost:3000');
-app.listen(3000, handleListen); 
+app.listen(3000, handleListen);

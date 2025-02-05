5e4e46a4ee4804cff62d4746fc0bccafb3b59dc7
younbihn <younbihn@naver.com>
2025-02-05 16:37:12 +0900

    sockets message send/receive

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,8 +1,12 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
 const socket = new WebSocket(`ws://${window.location.host}`);
 
-socket.addEventListener("open", () => {
+function handleOpen() {
     console.log("Conneted to Server");
-});
+}
+
+socket.addEventListener("open", handleOpen);
 
 socket.addEventListener("message", (message) => {
     console.log("New message: ", message.data);
@@ -12,6 +16,16 @@ socket.addEventListener("close", () => {
     console.log("Disconneted from Server");
 });
 
-setTimeout(() => {
-    socket.send("hello! from the browser");
-}, 1000);
+// setTimeout(() => {
+//     socket.send("hello! from the browser");
+// }, 10000);
+
+function handleSubmit (event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(input.value);
+    input.value = "";
+    // console.log(input.value);
+}
+
+messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -16,14 +16,26 @@ const handleListen = () => console.log('Listening on http://localhost:3000');
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+function onSocketClose() {
+    console.log("Disconnected from Browser");
+}
+
+const sockets = [];
+
 wss.on("connection", (socket) => {
-    // console.log(socket);
+    sockets.push(socket);
     console.log("Conneted to Browser");
-    socket.on("close", () => console.log("Disconnected from Browser"));
-    socket.on("message", message => {
-        console.log(message.toString());
+    socket.on("close", onSocketClose);
+    socket.on("message", (message) => {
+        if (message instanceof Buffer) {
+            const text = message.toString("utf-8");
+            console.log("Received:", text);
+            sockets.forEach(aSocket => aSocket.send(text));
+        } else {
+            console.log("Received non-buffer message:", message);
+            sockets.forEach(aSocket => aSocket.send(text));
+        }
     });
-    socket.send("hello");
 });
 
 server.listen(3000, handleListen);

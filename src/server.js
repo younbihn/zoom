d74f8ef14569a298f6d6e8a3ad4a383e426f37dc
younbihn <younbihn@naver.com>
2025-02-05 15:27:19 +0900

    Server - Client 메세지 test

--- src/server.js
+++ src/server.js
@@ -1,12 +1,29 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
+import { Socket } from "dgram";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res. render ("home"));
-app.get("/*", (req, res) => res. redirect("/"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log('Listening on http://localhost:3000');
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+    // console.log(socket);
+    console.log("Conneted to Browser");
+    socket.on("close", () => console.log("Disconnected from Browser"));
+    socket.on("message", message => {
+        console.log(message.toString());
+    });
+    socket.send("hello");
+});
+
+server.listen(3000, handleListen);

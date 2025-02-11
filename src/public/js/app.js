30cadc2e682df42a999412736c0fcadaaa3030f2
younbihn <younbihn@naver.com>
2025-02-11 12:14:25 +0900

    add room list

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -56,14 +56,27 @@ function handleRoomSubmit(event) {
 form.addEventListener("submit", handleRoomSubmit);
 
 socket.on("welcome", (user) => {
-    addMessage("Someone joined!");
+    addMessage(`${user} joined!`);
 })
 
 socket.on("bye", (left) => {
-    addMessage("Someone left");
+    addMessage(`${left} left`);
 })
 
 socket.on("new_message", addMessage);
+
+socket.on("room_change", (rooms) => {
+    const roomList = welcome.querySelector("ul");
+    roomList.innerHTML = "";
+    if (rooms.length === 0) {
+        return;
+    }
+    rooms.forEach((room) => {
+        const li = document.createElement("li");
+        li.innerText = room;
+        roomList.append(li);
+    });
+});
 
 //WebSocket
 // const messageList = document.querySelector("ul");

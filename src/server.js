c675e19c5cc57cc4e9a477ea15a923e6aecba32c
younbihn <younbihn@naver.com>
2025-02-11 12:52:54 +0900

    add admin-ui

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import http from "http";
 import express from "express";
-import SocketIO from "socket.io";
-import { disconnect } from "process";
+import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 
 const app = express();
 
@@ -14,7 +14,16 @@ app.get("/*", (req, res) => res.redirect("/"));
 const handleListen = () => console.log('Listening on http://localhost:3000');
 
 const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer);
+const wsServer = new Server(httpServer, {
+    cors: {
+      origin: ["https://admin.socket.io"],
+      credentials: true
+    }
+  });
+  instrument(wsServer, {
+    auth: false,
+    mode: "development",
+  });
 
 function onSocketClose() {
     console.log("Disconnected from Browser");

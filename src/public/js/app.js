5a9920425d7b3e362ad0bc8e7a2d1ff7314e7d67
younbihn <younbihn@naver.com>
2025-02-22 19:50:18 +0900

    add datachannel

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -15,6 +15,7 @@ let muted = false;
 let cameraOff = false;
 let roomName;
 let myPeerConnection;
+let myDataChannel;
 
 async function getCameras() {
     try {
@@ -123,6 +124,9 @@ welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 // Socket Code
 
 socket.on("welcome", async () => {
+    myDataChannel = myPeerConnection.createDataChannel("chat");
+    myDataChannel.addEventListener("message",console.log);
+    console.log("made data channel");
     const offer = await myPeerConnection.createOffer();
     myPeerConnection.setLocalDescription(offer);
     console.log("Send the offer");
@@ -130,6 +134,10 @@ socket.on("welcome", async () => {
 });
 
 socket.on("offer", async(offer) => {
+    myPeerConnection.addEventListener("datachannel", (event) => {
+        myDataChannel = event.channel;
+        myDataChannel.addEventListener("message", console.log);
+    });
     console.log("received the offer");
     myPeerConnection.setRemoteDescription(offer);
     const answer = await myPeerConnection.createAnswer();
@@ -154,11 +162,11 @@ function makeConnection() {
         iceServers: [
             {
                 urls: [
-                    "stun.l.google.com:19302",
+                    // "stun.l.google.com:19302",
                     "stun:stun1.l.google.com:19302",
                     "stun:stun2.l.google.com:19302",
                     "stun:stun3.l.google.com:19302",
-                    "stun:stun4.l.google.com:19302",
+                    "stun:stun4.l.google.com:19302"
                 ],
             },
         ],
